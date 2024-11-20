53b14ac73a7944dae2bd637811435f7d1c3c2642
minhpop <minhle0225@gmail.com>
2024-11-19 21:25:34 -0800

    add to photostrips

--- WebJam2024/app.js
+++ WebJam2024/app.js
@@ -20,3 +20,37 @@ function handleFiles() {
         }
     }
 }
+
+// Array of image paths
+const images = [
+    "./images/photostrip_double.png",
+    "./images/photostrip_horiz.png",
+    "./images/photostrip_horiz2.png",
+    "./images/photostrip_horiz3.png",
+    "./images/photostrip_single.png",
+    "./images/photostrip_single2.png",
+    "./images/photostrip_vert.png"
+];
+  
+  let currentIndex = 0;
+  
+  // Elements
+  const sliderImage = document.getElementById('sliderImage');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+  
+  // Update the image in the slider
+  function updateImage() {
+    sliderImage.src = images[currentIndex];
+  }
+  
+  // Event Listeners
+  prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+    updateImage();
+  });
+  
+  nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+    updateImage();
+  });
