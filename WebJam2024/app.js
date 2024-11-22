3794d84ed78e5a0823d9d893f483d72a690ae827
zhcaury2 <zhcaury2@gmail.com>
2024-11-21 19:26:53 -0800

    Added functionality-dragging and dropping images

--- WebJam2024/app.js
+++ WebJam2024/app.js
@@ -1,7 +1,9 @@
 const fileInput = document.getElementById('file-input')
 const photoBank = document.getElementById('photo-bank')
+const photo_bank_box = document.querySelector('#photo-bank .list')
 
 fileInput.addEventListener('change', handleFiles)
+photoBank.addEventListener('mousedown', dragDropFiles)
 
 function handleFiles() {
     const files = fileInput.files;
@@ -11,12 +13,48 @@ function handleFiles() {
             const reader = new FileReader();
             reader.readAsDataURL(file);
             reader.onload = () => {
-                const img = document.createElement('img');
+                const img = document.createElement('img')
                 img.src = reader.result;
-                photoBank.appendChild(img);
+                img.draggable = "true"
+                photo_bank_box.appendChild(img);
+                if (photo_bank_box.children.length%4 == 0){
+                    photoBank.style.minHeight+=100;
+                    photo_bank_box.appendChild(img);
+                } 
+
             };
         } else {
             alert(`${file.name} is not an image file.`);
         }
+    }
+}
+function dragDropFiles() {
+    let lists = document.getElementsByClassName("list")
+    let rightBox = document.querySelectorAll('#drop-zone .list')
+    let photoBox = document.querySelector('#photo-bank .list')
+
+    for(list of lists){
+        list.addEventListener("dragstart", function(e){
+            let selected = e.target;
+            
+            rightBox.forEach(element => {
+                element.addEventListener("dragover", function(e){
+                    e.preventDefault();
+                });
+                element.addEventListener("drop", function(e){
+                    if (element.children.length == 0){
+                        element.appendChild(selected);
+                    }
+                    selected = null;
+                });
+        });
+            photoBox.addEventListener("dragover", function(e){
+                e.preventDefault();
+            });
+            photoBox.addEventListener("drop", function(e){
+                photoBox.appendChild(selected);
+                selected = null;
+            });
+        })
     }
 }
